7fc416f48beddf4a882ae63f60bf21c274f71015
Amar Idrizovic <amari@amars-imac.qssbh.com>
2015-07-27 13:11:27 +0200

    Names with Faces

---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Amar Idrizovic on 27/07/15.
+//  Copyright (c) 2015 Amar Idrizovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
