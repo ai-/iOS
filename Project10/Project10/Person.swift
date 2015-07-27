0044ebb5609ac00e55d1c9b3aa5054c3d297317d
Amar Idrizovic <amari@amars-imac.qssbh.com>
2015-07-27 14:36:31 +0200

    Names with Faces

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Amar Idrizovic on 27/07/15.
+//  Copyright (c) 2015 Amar Idrizovic. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+   
+}
---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,18 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Amar Idrizovic on 27/07/15.
+//  Copyright (c) 2015 Amar Idrizovic. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+    
+    
+}
